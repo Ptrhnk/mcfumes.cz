c812f9890b34f0f61eb21ac148d85c32c070592a
Petr Hanak <petr.nahak@gmail.com>
2021-09-28 16:13:54 +0200

    improve player, add loaders

--- components/Audio/AudioPlayer/AudioPlayer.js
+++ components/Audio/AudioPlayer/AudioPlayer.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { useAudioPlayer } from "react-use-audio-player";
+import { AiOutlinePlayCircle, AiOutlinePauseCircle } from "react-icons/ai";
+import {
+  IoPlaySkipBackCircleOutline,
+  IoPlaySkipForwardCircleOutline
+} from "react-icons/io5";
+
+import { VscLoading } from "react-icons/vsc";
+import { BsSkipEndFill, BsSkipStartFill } from "react-icons/bs";
+import { FiSkipBack, FiSkipForward } from "react-icons/fi";
 
 const tracklist = [
   { path: "/songs/nlb/01_intro.mp3", name: "Intro" },
@@ -13,30 +23,86 @@ const tracklist = [
   { path: "/songs/nlb/09_hvezda_skit.mp3", name: "Hvězda skit" },
   { path: "/songs/nlb/10_outro.mp3", name: "Outro" }
 ];
+const StyledAudioPanel = styled.div`
+  position: absolute;
+  bottom: 0;
+  /* left: 0; */
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(50, 50, 50, 0.5);
+  border-top-left-radius: 1rem;
+  /* height: 10rem; */
+  width: 30rem;
+  padding: 1.5rem;
+`;
+const StyledBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledTrackTitle = styled.div`
+  font-size: 1.6rem;
+  color: white;
+  margin-bottom: 1rem;
+`;
+const StyledIconButton = styled.button`
+  background: none;
+  color: white;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  padding: 0 0.3rem;
+`;
 export const AudioPlayer = () => {
   const [currentTrack, setCurrentTrack] = useState(0);
+
+  const { togglePlayPause, ready, loading, playing, error, volume, play } =
+    useAudioPlayer({
+      src: tracklist[currentTrack].path,
+      format: ["mp3"],
+      autoplay: false
+    });
+
   const setNextTrack = () => {
     currentTrack < tracklist.length - 1
       ? setCurrentTrack(currentTrack + 1)
       : setCurrentTrack(0);
+    play();
+  };
+  const setPreviousTrack = () => {
+    currentTrack > 0
+      ? setCurrentTrack(currentTrack - 1)
+      : setCurrentTrack(tracklist.length - 1);
+    play();
   };
 
-  const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
-    src: tracklist[currentTrack].path,
-    format: ["mp3"],
-    autoplay: false,
-    onend: () => console.log("sound has ended!")
-  });
-
-  // if (!ready && !loading)
-  //   return <div style={{ color: "white" }}>No audio to play</div>;
-  // if (loading) return <div style={{ color: "white" }}>Loading audio</div>;
-
   return (
-    <div>
-      <button onClick={togglePlayPause}>{playing ? "Pause" : "Play"}</button>
-      <button onClick={() => setNextTrack()}>{">>"}</button>
-      <p style={{ color: "white" }}>{tracklist[currentTrack].name}</p>
-    </div>
+    <StyledAudioPanel>
+      <StyledBox>
+        <StyledTrackTitle>{tracklist[currentTrack].name}</StyledTrackTitle>
+      </StyledBox>
+      <StyledBox>
+        <StyledIconButton onClick={() => setPreviousTrack()}>
+          <BsSkipStartFill size={22} />
+        </StyledIconButton>
+        <StyledIconButton onClick={togglePlayPause}>
+          {!ready && !error && loading ? (
+            <VscLoading size={36} />
+          ) : playing ? (
+            <AiOutlinePauseCircle size={36} />
+          ) : (
+            <AiOutlinePlayCircle size={36} />
+          )}
+        </StyledIconButton>
+        <StyledIconButton onClick={() => setNextTrack()}>
+          <BsSkipEndFill size={22} />
+        </StyledIconButton>
+      </StyledBox>
+    </StyledAudioPanel>
   );
 };
